0b3b651716775db44eef1d1869246214ed53bec9
Misael.K <misael_k@yahoo.com.ar>
2015-07-09 20:08:03 -0300

    Terminado de adaptar PID de Tito, con algunas mejoras. Emprolijamiento general. Se acomodaron declaraciones, se quitaron números mágicos, y otros ajustes.

--- SRC/arion/arion.ino
+++ SRC/arion/arion.ino
@@ -1,40 +1,34 @@
-//Este es el codigo para arion. 
-
-//Definicion de pines del micro.
+// definición de pines del micro.
 const int pwmMotorD = 11;
 const int pwmMotorI = 10;
-
 const int sentidoMotorD = 3;
 const int sentidoMotorI = 5;
-
+const int ledArduino = 13;
+const int led1 = 9;
+const int led2 = 8;
+const int led3 = 12;
+const int boton1 = 4;
+const int boton2 = 6;
+const int boton3 = 7;
 const int sensor0 = A0;
 const int sensor1 = A1;
 const int sensor2 = A2;
 const int sensor3 = A3;
 const int sensor4 = A4;
-
 const int batteryControl = A5;
 
-const int ledArduino = 13;
-const int led1 = 9;
-const int led2 = 8;
-const int led3 = 12;
+const int cantidadDeSensores = 5;
+int sensores[cantidadDeSensores];
 
-const int boton1 = 4;
-const int boton2 = 6;
-const int boton3 = 7;
+// indices de array sensores
+const int izq    = 0;
+const int cenIzq = 1;
+const int cen    = 2;
+const int cenDer = 3;
+const int der    = 4;
 
-//Declaracion de constantes para el codigo.
-const int tolerancia = 5; //Margen de ruido al medir negro.
-const int MAX_SENSORES = 5;
-
-int sensores[MAX_SENSORES];
-
-boolean estadoActualAdentro;
-
-int ultimoBorde;
-const int derecha = 1;
-const int izquierda = 0;
+const int tolerancia = 5; // Margen de ruido al medir negro.
+const int toleranciaBorde = 50; // Mínimo para decidir cuál fue el último borde
 
 // velocidadMinima + rangoVelocidad <= 255 (o explota)
 const int velocidadMinima = 0;
@@ -51,21 +45,34 @@ const int coeficienteErrorP = 12;
 const int coeficienteErrorI = 6000;
 const int coeficienteErrorD = 3;
 
-void setup() {                
+boolean estadoActualAdentro; // determina si se usa modo PID o modo "me fui"
+// bordes para modo "me fui"
+const int derecha = 1;
+const int izquierda = 0;
+int ultimoBorde;
 
-  pinMode(pwmMotorD, OUTPUT);
-  pinMode(pwmMotorI, OUTPUT);
+// tiempo entre ciclos de PID.
+// no debe ser 0, pues se usa para dividir
+long tiempo = 1;
+
+void setup() {                
+  // como los motores se manejan con AnalogWrite, 
+  // no hace falta ponerlos como salida
+  // pinMode(pwmMotorD, OUTPUT);
+  // pinMode(pwmMotorI, OUTPUT);
 
   pinMode(sentidoMotorD, OUTPUT); 
   pinMode(sentidoMotorI, OUTPUT); 
   
-//  pinMode(sensor0, INPUT);
-//  pinMode(sensor1, INPUT);
-//  pinMode(sensor2, INPUT);
-//  pinMode(sensor3, INPUT);
-//  pinMode(sensor4, INPUT);
-
-//  pinMode(batteryControl, INPUT);
+  // como los sensores y el batteryControl se leen con AnalogRead, 
+  // no hace falta ponerlos como entrada
+  // pinMode(sensor0, INPUT);
+  // pinMode(sensor1, INPUT);
+  // pinMode(sensor2, INPUT);
+  // pinMode(sensor3, INPUT);
+  // pinMode(sensor4, INPUT);
+  // pinMode(batteryControl, INPUT);
+  
   pinMode(ledArduino, OUTPUT);
   pinMode(led1, OUTPUT);  
   pinMode(led2, OUTPUT);  
@@ -74,37 +81,43 @@ void setup() {
   pinMode(boton1, INPUT);
   pinMode(boton2, INPUT);
   pinMode(boton3, INPUT);
-  
-  estadoActualAdentro = false;
-  ultimoBorde = izquierda;
-  
-  for (int i = 0; i < MAX_SENSORES; i++) {
+
+  for (int i = 0; i < cantidadDeSensores; i++) {
     sensores[i] = 0;
   }
+  
+  apagarMotores();
+  
+  errP = 0;
+  errI = 0;
+  errD = 0;
+  errPAnterior = 0;
+  ultimoBorde = izquierda;
+  estadoActualAdentro = true;
+  tiempo = 1;
+  
 }
 
-boolean boton1Apretado() {
-  return (digitalRead(boton1) == LOW);
+boolean apretado(int boton) {
+  return (digitalRead(boton) == LOW);
 }
 
-boolean boton2Apretado() {
-  return (digitalRead(boton2) == LOW);
+void esperarReboteBoton() {
+  delay(5);
 }
 
-boolean boton3Apretado() {
-  return (digitalRead(boton3) == LOW);
-}
-
-void obtenerSensores() {
-  sensores[0] = analogRead(sensor0);
-  sensores[1] = analogRead(sensor1);
-  sensores[2] = analogRead(sensor2);
-  sensores[3] = analogRead(sensor3);
-  sensores[4] = analogRead(sensor4);
+inline void obtenerSensores() {
+  // carga en el array de sensores las lecturas AD de cada sensor
+  // este proceso lleva 500 us
+  sensores[izq]    = analogRead(sensor0);
+  sensores[cenIzq] = analogRead(sensor1);
+  sensores[cen]    = analogRead(sensor2);
+  sensores[cenDer] = analogRead(sensor3);
+  sensores[der]    = analogRead(sensor4);
 }
 
 void mostrarSensor(int sensor) {
-  if ((sensor >= MAX_SENSORES) || (sensor < 0)) {
+  if ((sensor >= cantidadDeSensores) || (sensor < 0)) {
     return; 
   }
   digitalWrite(led1, ((sensores[sensor] / 256) ? HIGH : LOW));
@@ -119,36 +132,44 @@ void apagarMotores() {
 
 void loop() {
 
-  while (!boton2Apretado()) {
+  // hasta que se presione el botón, espera,
+  // y muestra en los leds el valor del sensor central
+  while (!apretado(boton2)) {
     obtenerSensores();
-    mostrarSensor(2);
+    mostrarSensor(cen);
   }
-  delay(5); //rebote botón
+  esperarReboteBoton();
 
-  while (boton2Apretado());
-  delay(5); //rebote botón
+  // inicialización de todo
+  setup();
 
-  // inicializacion variables pid
-  errP = 0;
-  errPAnterior = 0;
-  errI = 0;
-  errD = 0;
-  ultimoBorde = izquierda;
-  estadoActualAdentro = true;
-  
-  // arranque progresivo
+  // hasta que se suelte el botón, espera 
+  while (apretado(boton2));
+  esperarReboteBoton();
 
-  while (!boton2Apretado()) {
+  // arranque progresivo, de 0 a 250 en 75 ms
+  for (int i = 1; i <= 25; i++) {
+    analogWrite(pwmMotorD, i * 10);
+    analogWrite(pwmMotorI, i * 10);
+    delay(3);
+  }
+
+  // ejecuta el ciclo principal hasta que se presione el botón
+  while (!apretado(boton2)) {
     obtenerSensores();
     
-    if (sensores[0] > 50) {
+    if (sensores[izq] > toleranciaBorde) {
       ultimoBorde = izquierda;
-    } else if (sensores[4] > 50) {
+    } else if (sensores[der] > toleranciaBorde) {
       ultimoBorde = derecha;
     }
 
     // si me fui, entro en modo "corrección máxima"
-    if ((sensores[0] < tolerancia) && (sensores[1] < tolerancia) && (sensores[2] < tolerancia) && (sensores[3] < tolerancia) && (sensores[4] < tolerancia)) {
+    if ((sensores[izq]    < tolerancia) && 
+        (sensores[cenIzq] < tolerancia) && 
+        (sensores[cen]    < tolerancia) && 
+        (sensores[cenDer] < tolerancia) && 
+        (sensores[der]    < tolerancia)) {
       estadoActualAdentro = false;
     } else {
       estadoActualAdentro = true;
@@ -157,20 +178,30 @@ void loop() {
     if (estadoActualAdentro) {
       // modo pid
       // linea = (0 * s0 + 1000 * s1 + 2000 * s2 + 3000 * s3 + 4000 * s4) / (s0 + s1 + s2 + s3 + s4)
-      // 0 a 4000, donde 2000 es el centro
-      sensoresLinea = ((int32_t)sensores[1] * 1000 + (int32_t)sensores[2] * 2000 + (int32_t)sensores[3] * 3000 + (int32_t)sensores[4] * 4000) / 
-                      ((int32_t)sensores[0] + (int32_t)sensores[1] + (int32_t)sensores[2] + (int32_t)sensores[3] + (int32_t)sensores[4]);
+      // 0 a 4000, donde 2000 es el centroDeLinea
+      sensoresLinea = (
+        (int32_t)sensores[cenIzq] * 1000 + 
+        (int32_t)sensores[cen]    * 2000 + 
+        (int32_t)sensores[cenDer] * 3000 + 
+        (int32_t)sensores[der]    * 4000
+      ) / (
+        (int32_t)sensores[izq]    + 
+        (int32_t)sensores[cenIzq] + 
+        (int32_t)sensores[cen]    + 
+        (int32_t)sensores[cenDer] + 
+        (int32_t)sensores[der]
+      );
   
       errP = sensoresLinea - centroDeLinea;
-      
-      errD = errP - errPAnterior;
-      //err_i += (err_p >> 8);
+      errI = errP * tiempo;
+      errD = (errP - errPAnterior) / tiempo;
+      // err_i += (err_p >> 8);
       //if ( (err_i >= VALOR_MAX_INT16 - VALOR_MAX_ERR_P) || (err_i <= -(VALOR_MAX_INT16 - VALOR_MAX_ERR_P)) ) {
       //    err_i -= (err_p >> 8);
       //}
       errPAnterior = errP;
 
-      reduccionVelocidad = errP * ( 1 / coeficienteErrorP); //+ err_i * COEFICIENTE_ERROR_I + err_d * COEFICIENTE_ERROR_D;
+      reduccionVelocidad = errP * ( 1 / coeficienteErrorP) + errI * (1 / coeficienteErrorI) + errD * (1 / coeficienteErrorD);
       
       // printf de valores PID
       //printf("p:%5i i:%5i d:%5i rv:%5i\n", err_p, err_i, err_d, reduccion_velocidad);
@@ -194,6 +225,8 @@ void loop() {
         analogWrite(pwmMotorD, velocidadMinima + rangoVelocidad - reduccionVelocidad);
       }
       
+      tiempo = micros() - tiempo;
+      
     } else {
       // modo me fui
       if (ultimoBorde == izquierda) {
@@ -205,12 +238,14 @@ void loop() {
       }
     }
   }
+  esperarReboteBoton();
   
-  // fin de tareas, para poder empezar de nuevo
+  // inmediatamente después de presionar el botón para salir del ciclo, 
+  // se apagan los motores
   apagarMotores();
-  delay(5); //rebote botón
 
-  while (boton2Apretado());
-  delay(5); //rebote botón
+  // hasta que se suelte el botón, espera
+  while (apretado(boton2));
+  esperarReboteBoton();
 
 }
